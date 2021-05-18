949fcceb0573e5105742980754ed67cd0cb28a34
zoli5 <szzoltan2004@gmail.com>
2021-05-18 20:08:10 +0200

    mg tisztitott

---
+++ d1di2lzuh97fh2.cloudfront.net/client.fe/js.compiled/lang.hu.478dbc2.js
@@ -0,0 +1,14 @@
+// Copyright 2006 The Closure Library Authors. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS-IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+window.translations=["Blog","Termékek","Jelenleg nem találhatóak blogbejegyzések. Újat az “Új bejegyzés” gombra kattintva hozhatsz létre.","Még nincsenek termékek. Kattints a \"Termék hozzáadása\" lehetőségre.","Nincs megjeleníthető blogbejegyzés. Hozz létre \"Blog\" típusú oldalt.","Ez a kategória üres. Hozz létre termékeket, vagy válassz másik kategóriát.","Képgaléria","Ez a képgaléria nem tartalmaz képeket.","Ingyenes","Mini","Standard","Profi","Limited"];
